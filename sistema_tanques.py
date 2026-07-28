0d3a24ddf14db554d4366b684a82dceaea5b02b5
luismaso <luishenriquemaso@gmail.com>
2026-07-27 23:41:47 -0300

    validação de lotes com a média do pH dos tanques

--- sistema_tanques.py
+++ sistema_tanques.py
@@ -1,5 +1,3 @@
-opcaomenu = ""
-
 def coletar_ph_tanques(linhas, colunas):
     matriz = []
 
@@ -18,9 +16,27 @@ def coletar_ph_tanques(linhas, colunas):
                 except ValueError:
                     print("Valor inválido. Por favor, digite um número.")
         matriz.append(linha)
+    return matriz   
 
-    return matriz
+def validar_lotes(matriz): 
+    vetor_status = []
+    for i in range(len(matriz)):
+        soma = sum(matriz[i])
+        quantidade = len(matriz[i])
+        media = soma / quantidade
 
+        if 6.5 <= media <= 7.5:
+            status = 1
+            print(f"Tanque {i+1}: Média = {media:.2f} - Status: Aprovado\n\n")
+        else:
+            status = 0 
+            print(f"Tanque{i+1}: Média = {media:.2f} - Status: Reprovado\n\n")
+        vetor_status.append(status)
+
+    return vetor_status
+    
+
+opcaomenu = ""
 while opcaomenu != "4":
     print("╔══════════════════════════════════════╗")
     print("║     SISTEMA DE CONTROLE DE TANQUES   ║")
@@ -41,10 +57,12 @@ while opcaomenu != "4":
         print("\nMatriz salva com sucesso!")
         print(matriz_tanques, "\n")
        
-
-
     elif opcaomenu == "2":
-        print("teste 222222\n")
+        if len(matriz_tanques) == 0:
+            print("Nenhuma matriz de tanques foi coletada.")
+        else:
+            vetor_status = validar_lotes(matriz_tanques)
+                  
     elif opcaomenu == "3": 
         print("teste 333333\n")
     elif opcaomenu == "4":
